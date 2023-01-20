ded6acf9ae52e7bd4748cd2aab3cdf4cdfb78613
magda_dev <30729081+Magda-dev@users.noreply.github.com>
2023-01-20 13:25:39 +0100

    [ADD] Creation du formulaire en js

--- assets/JS/test.js
+++ assets/JS/test.js
@@ -1,289 +1,148 @@
-function getFieldValue(elementId) {
-    var element = document.getElementById(elementId);
-    return element.value || "";
+const btnAdd = document.createElement('button');
+btnAdd.innerHTML = "Ajouter un Livre";
+btnAdd.classList.add('myBtn');
+var content = document.querySelector('#content');
+var deleteBtn = document.createElement("button");
+var searchBookBtn = document.createElement("button");
+var inputTitle = document.createElement("input");
+var inputAuthor = document.createElement("input");
+
+content.appendChild(btnAdd);
+btnAdd.addEventListener('click', showSearchBookForm);
+
+
+
+  function showSearchBookForm() {
+    // retrait du bouton ajouter un livre
+    btnAdd.classList.add('hidden');
+
+    //création de la div pour le formulaire
+    const formSection = document.createElement("div");
+    formSection.classList.add('modal');
+    formSection.innerHTML = "Rechercher un livre";
+
+    //creation du formulaire
+    const form = document.createElement("form")
+    form.classList.add('search_form');
+
+    //création de l'element input titre du livre
+    var labelTitle = document.createElement("label");
+    labelTitle.innerHTML = "Titre :";
+    inputTitle.setAttribute("type", "text");
+    inputTitle.setAttribute("name", "title");
+    inputTitle.setAttribute("placeholder", "Titre");
+    inputTitle.id ='title';
+
+
+    //création de l'element input auteur du livre
+    
+    var labelAuthor = document.createElement("label");
+    labelAuthor.innerHTML = "Auteur :";
+    inputAuthor.setAttribute("type", "text");
+    inputAuthor.setAttribute("name", "author");
+    inputAuthor.setAttribute("placeholder", "Auteur");
+    inputTitle.id ='author';
+
+
+    // création du bouton de recherche
+    
+    searchBookBtn.innerHTML = "Rechercher";
+    searchBookBtn.classList.add('myBtn');
+    //searchBookBtn.type = "submit";
+    searchBookBtn.style.width = "20%";
+    
+  
+    // Création du bouton annuler
+    deleteBtn.innerHTML = "Annuler";
+    deleteBtn.classList.add('myBtn');
+    deleteBtn.id = 'cancel';
+    deleteBtn.type = "reset";
+    deleteBtn.style.width = "20%";
+
+    // Ajout des divers élements dans le formulaire
+    form.appendChild(labelTitle);
+    form.appendChild(inputTitle);
+    form.appendChild(labelAuthor);
+    form.appendChild(inputAuthor);
+    form.appendChild(searchBookBtn);
+    form.appendChild(deleteBtn);
+
+    // ajout du formulaire à la div
+    formSection.appendChild(form);
+
+    // Ajouter de la div dans le document
+    content.appendChild(formSection);
+    searchBookBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(inputTitle.value);
+        console.log(inputAuthor.value);
+        searchBook();
+      }
+     );
   }
-  
-  function clearElement(parentId) {
-    var element = document.getElementById(parentId);
-    if (element) {
-      element.innerHTML = "";
-    }
-  }
-  function addElement(parent, tag, attributes) {
-    var element = document.createElement(tag);
-  
-    // Affecter les attributs au nouvel élément à créer
-    for (key of Object.keys(attributes)) {
-      element[key] = attributes[key];
-    }
-  
-    var parentDOM = document.getElementById(parent) || parent;
-    parentDOM.appendChild(element);
-    return element;
+  function getFieldValue(inputId) {
+    var inputElement = document.getElementById(inputId);
+    return inputElement.value || "";
   }
 
-  
-  function showSearchBookForm() {
-    // Effacer contenu de la recherche
-    clearElement("searchForm");
-  
-    const formDiv = addElement("searchForm", "form", {
-      id: "newForm",
-      action: "",
-      onsubmit: function () {
-        searchBookOnGoogle({
-          title: getFieldValue("title"),
-          author: getFieldValue("author"),
-        });
-        return false;
-      },
-    });
-  
-    // Creer champs du formulaire
-    addElement(formDiv, "input", {
-      type: "text",
-      name: "title",
-      placeholder: "Titre du livre",
-      id: "title",
-      required: true,
-    });
-  
-    addElement(formDiv, "input", {
-      type: "text",
-      name: "author",
-      placeholder: "Auteur du livre",
-      id: "author",
-      required: true,
-    });
-  
-    // Ajouter bouton de rechérche
-    var searchBookButton = document.createElement("button");
-    searchBookButton.innerHTML = "Rechercher";
-    searchBookButton.type = "submit";
-    searchBookButton.style.backgroundColor = "#009966";
-    searchBookButton.style.margin = "8%";
-    searchBookButton.style.width = "35%";
-    // Ajouter le bouton de rechercher sur la page
-    formDiv.appendChild(searchBookButton);
-  
-    // Ajouter bouton annuler
-    var deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Annuler";
-    deleteButton.type = "reset";
-    deleteButton.style.backgroundColor = "#FF0000";
-    deleteButton.style.margin = "8%";
-    deleteButton.style.width = "35%";
-    deleteButton.onclick = function () {
-      // Effacer contenu de la recherche
-      clearElement("searchResults");
-      return true;
+function searchBook(){
+var book ={
+  title : getFieldValue("title"),
+  author : getFieldValue("author"),
+};
+var bookList =[];
+book.title = inputTitle.value;
+book.author = inputAuthor.value;
+
+var url="https://www.googleapis.com/books/v1/volumes?AIzaSyBPzxg5cRwtZ9C-dyJqIAegQIHhngSHPdQ&q="+ book.title+ "" + book.author;
+
+
+//appel de l'API Google Books (GET)
+//asynchrone pour laisser le temps d'aller chercher les films avant d'exécuter le reste du code sinon pas sde données 
+const fetchBooks = async () => {
+      bookList = await fetch(url).then((response) => response.json());
+
+
+    //une fois le resultat obtenu on converti le resulat json
+    //const books = await response.json();
+    console.log(bookList);
+  };
+  var outputList = document.querySelector("#content");
+  var container =
+    `<div>
+    <h2>Résultat de recherche</h2>
+    
+    <div id="searchResults">
+      <ul id="book_list">
+      </ul>
+      </div>
+    `;
+    outputList.insertAdjacentHTML('afterbegin',container);
+    console.log(outputList);
+
+  var bookDisplay = async () => {
+    await fetchBooks();
+    for(var i=0; i<bookList.length;i++){
+    var myBooks = 
+    `
+    <li id="bookItem">
+      <h3>${book.title}</h3>
+      <p>${book.author}</p>
+      <img src="" alt="">
+    
+    </li>
+    `
+
+  }
+    
+
+    container.insertAdjacentHTML('afterbegin', myBooks)
     };
-    //Ajouter le bouton delete sur ma page
-    formDiv.appendChild(deleteButton);
+
+
   
-    var addBookButton = document.createElement("button");
-    addBookButton.innerHTML = "Ajouter ce livre";
-    addBookButton.onclick = function () {
-      //fonction d'ajout du livre
-      addBookButton.innerHTML = document.getElementById("bookAddAction");
-      addBookInList({
-        title: titleBookInput.value,
-        author: authorBookInput.value,
-      });
-      return false;
-    };
-  }
+  fetchBooks();
   
-  function searchBookOnGoogle(book) {
-    // 1. Formatter l'URL de recherche Google avec les paramètres de l'objet book
-    let googleBookAPIURL =
-      "https://www.googleapis.com/books/v1/volumes?q=" +
-      "+intitle:" +
-      book.title +
-      "" +
-      "+inauthor:" +
-      book.author;
-  
-    // console.log(googleBookAPIURL);
-  
-    // 2. Lancer la requête
-    $.ajax({
-      url: googleBookAPIURL.toString(),
-      dataType: "jsonp",
-      crossDomain: true,
-      success: function (data) {
-        // 3. Récupérer les résultats
-        updateSearchResults(data);
-      },
-    });
-  }
-  
-  function removeBookFromPochList(book) {
-    const bookIDToRemove = book.id;
-    // Retirer du DOM
-    const divBook = document.getElementById("myBooks-" + bookIDToRemove);
-    divBook.remove();
-    // Retirer du session storage
-    const booksInSession = sessionStorage.getItem("books");
-    if (booksInSession) {
-      const booksInSessionArray = booksInSession.split("|");
-      const booksInSessionWithoutRemovedItem = booksInSessionArray.filter(
-        (currentBookJsonEncoded) =>
-          //Utiliser la librairie JSON pour récup l'Id du livre
-          (JSON.parse(currentBookJsonEncoded) || {}).id !== book.id
-      );
-      sessionStorage.setItem("books", booksInSessionWithoutRemovedItem.join("|"));
-    }
-  }
-  
-  function addBookToPochList(book, checkIfExisting) {
-    let bookListInSessionStorage = sessionStorage.getItem("books");
-    let bookExists = false;
-  
-    if (checkIfExisting) {
-      if (bookListInSessionStorage) {
-        const booksInSession = bookListInSessionStorage.split("|");
-        const existingBooksInSession = booksInSession.filter(
-          (currentBookJsonEncoded) =>
-            (JSON.parse(currentBookJsonEncoded) || {}).id === book.id
-        );
-        if (existingBooksInSession.length) {
-          // Vérifier que le book n'est pas déjà dans la session
-          // Si pas déjà présent ==> l'ajouter au session storage
-          bookExists = true;
-        }
-      }
-    }
-  
-    if (!bookExists) {
-      // Ajouter au session storage
-      const bookData = JSON.stringify(book);
-      if (bookListInSessionStorage) {
-        bookListInSessionStorage = bookListInSessionStorage + "|" + bookData;
-      } else {
-        bookListInSessionStorage = bookData;
-      }
-  
-      sessionStorage.setItem("books", bookListInSessionStorage);
-    }
-  
-    // Add book in poch list (myBooks) ; with no bookmarks
-    addBookItemInDOM("myBooks", book, false);
-  }
-  function viderPochList() {
-    clearElement("myBooks");
-  }
-  
-  function addBookItemInDOM(target, book, hasBookmark) {
-    // Créer div pour le book
-    const bookID = target + "-" + book.id;
-    addElement(target, "div", {
-      className: "book-item",
-      id: bookID,
-    });
-  
-    if (hasBookmark) {
-      // Créer icone bookmark pour le book (dans le conteneur du book)
-      addElement(bookID, "div", {
-        className: "book-bookmark",
-        innerHTML: "<img width='24' height='24' src='bookmark.png' />",
-        onclick: function () {
-          const checkBookInDOM = document.getElementById("myBooks-" + book.id);
-          if (!checkBookInDOM) {
-            addBookToPochList(book, true);
-          } else {
-            alert("“Vous ne pouvez pas ajouter deux fois le même livre");
-          }
-        },
-      });
-    } else {
-      // Créer icone suppression pour le book (dans le conteneur du book)
-      addElement(bookID, "div", {
-        className: "book-delete",
-        innerHTML:
-          "<image width ='24' height='24' src='Places-trash-empty-icon.png' />",
-        onclick: function () {
-          removeBookFromPochList(book);
-        },
-      });
-    }
-  
-    // Insérer le titre (dans le conteneur du book)
-    addElement(bookID, "div", {
-      className: "book-title",
-      innerHTML: "Titre: " + book.volumeInfo.title,
-    });
-  
-    // Insérer l'ID (dans le conteneur du book)
-    addElement(bookID, "div", {
-      className: "book-id",
-      innerHTML: "ID: " + book.id,
-    });
-  
-    // Insérer l'auteur (dans le conteneur du book)
-    addElement(bookID, "div", {
-      className: "book-author",
-      innerHTML: "Auteurs: " + (book.volumeInfo.authors || []).join(", "),
-    });
-  
-    // Insérer la description (dans le conteneur du book)
-    addElement(bookID, "div", {
-      className: "book-description",
-      innerHTML: book.volumeInfo.description
-        ? `Description:${book.volumeInfo.description}`
-        : "Informations manquantes",
-    });
-  
-    // Insérer l'imagine (dans le conteneur du book)
-    addElement(bookID, "div", {
-      className: "book-image",
-      innerHTML: book.volumeInfo.imageLinks
-        ? "<img width='100' height='100' src='" +
-          book.volumeInfo.imageLinks.thumbnail +
-          "' />"
-        : "<img width='100' height='100' src='unavailable.png' />",
-    });
-  }
-  
-  function updateSearchResults(data, limit) {
-    clearElement("searchResults");
-  
-    if (!limit) {
-      limit = 4;
-    }
-  
-    const items =
-      data.totalItems > 0 &&
-      data.items.filter(function (item, index) {
-        return index < limit;
-      });
-    if (data.totalItems > 0) {
-      // Si y'a plusieurs livres > 0, alors les afficher
-      for (const book of items) {
-        // Add book in search results ; with bookmarks
-        addBookItemInDOM("searchResults", book, true);
-      }
-    } else {
-      addElement("searchResults", "div", {
-        className: "no-search-result",
-        innerHTML: "Aucun livre n'a été trouvé",
-      });
-    }
-  }
-  
-  function loadBooksFromStorage() {
-    const books = sessionStorage.getItem("books");
-    if (books) {
-      // Si des livres sont présents dans la session storage
-      // Format "bookID1, bookID2"
-      const listeBooks = books.split("|");
-  
-      // Charger la poch list
-      for (const bookJSON of listeBooks) {
-        if (bookJSON) {
-          const bookParse = JSON.parse(bookJSON);
-          addBookToPochList(bookParse, true);
-        }
-      }
-    }
-  }
+  bookDisplay();
+}
+searchBookBtn.addEventListener('submit', searchBook);
