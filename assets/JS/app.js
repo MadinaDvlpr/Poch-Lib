4f71cb137397d1e3a65521598fc6ef207dabf6ce
magda_dev <30729081+Magda-dev@users.noreply.github.com>
2023-02-20 01:18:04 +0100

    [FIX] Check if existing
    making async

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -294,7 +294,7 @@ function truncate(description, maxlength) {
 function toggleIcon(icon){
   icon.classList.toggle("fa-trash");
 }
-function checkIfExisting(book){
+async function checkIfExisting(book){
   let bookInStorage = JSON.parse(sessionStorage.getItem("pochlist"));
   if (bookInStorage) {
     const existingBooks = bookInStorage.find((currentBook)=>currentBook.id == book.id)
